6285922ed048a4676ebf6d754d0d6d9931e16ee6
pragti gupta <pragtig6@gmail.com>
2024-11-30 13:29:10 +0530

    new

---
+++ src/pac1/String1.java
@@ -0,0 +1,16 @@
+package pac1;
+
+public class String1 {
+	
+	public static void main(String[] args) {
+		
+		
+		String str1="pragti";
+	
+		
+		
+		System.out.println(str1);
+		
+	}
+
+}
---
+++ src/pac1/String2.java
@@ -0,0 +1,19 @@
+package pac1;
+
+public class String2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="gupta";
+		
+	char[] arr=	str.toCharArray();
+	
+	for(char c:arr)
+	{
+		
+		System.out.println(c);
+	}
+
+	}		
+}
