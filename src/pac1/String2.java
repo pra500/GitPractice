5868567565fd7039333c4b256bad50b240b20946
pragti gupta <pragtig6@gmail.com>
2024-12-06 16:23:47 +0530

    f

--- src/pac1/String2.java
+++ src/pac1/String2.java
@@ -4,16 +4,24 @@ public class String2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		String str="gupta";
-		
-	char[] arr=	str.toCharArray();
 	
-	for(char c:arr)
-	{
-		
-		System.out.println(c);
-	}
+	
 
-	}		
+String str="prattgii";
+
+
+char[] arr=str.toCharArray();
+
+
+
+System.out.println(arr.length);
+
+	
+
+
+
+	
+	}	
+	
+	
 }
